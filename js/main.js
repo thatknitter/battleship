9add48f1c1614aa7b5fd2f1d7a3b92086d461742
thatknitter <kblewis.flt@gmail.com>
2014-11-20 15:41:24 -0600

    fixed test file

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-$(function(){
-  var $table = $('table');
-});
+// $(function(){
+  // var $table = $('table');
+// });
 
 function displayOutput() {
   return $('#displayoutput').val();
 }
 
-function boatGrid(){
-  this.grid = [];
-  for(i = 0; i < 10; i++) {
-    this.grid[i] = [];
-    for(j = 0; j < 10; j++){
-      this.grid[i][j] = [];
-    }
-  }
-  this.drawGrid = function(table, option){
-    
-  };
-}
+// function boatGrid(){
+  // this.grid = [];
+  // for(i = 0; i < 10; i++) {
+    // this.grid[i] = [];
+    // for(j = 0; j < 10; j++){
+      // this.grid[i][j] = [];
+    // }
+  // }
+  // this.drawGrid = function(table, option){
+//     
+  // };
+// }
--- spec/tests.js
+++ spec/tests.js
@@ -5,18 +5,18 @@
   describe('displaying the output', function() {
     it('should display the output', function() {
       $('#displayoutput').val(1.23);
-      assert.deepequal(displayOutput(), 1.23);
+      assert.equal(displayOutput(), 1.23);
       $('#displayoutput').val(2.34);
-      assert.deepequal(displayOutput(), 2.34);
+      assert.equal(displayOutput(), 2.34);
     });
   });
 
-  describe('building the grid', function() {
-    describe('building the boat grid', function() {
-      it('should return a 10x10 grid', function() {
+  // describe('building the grid', function() {
+    // describe('building the boat grid', function() {
+      // it('should return a 10x10 grid', function() {
+// 
+      // });
+    // });
+  // });
 
-      });
-    });
-  });
-
-});
+})();
