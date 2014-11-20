ba1626c554fa5a7e04a6306296bee3d6ed39977c
thatknitter <kblewis.flt@gmail.com>
2014-11-20 15:29:30 -0600

    initial test written, errors fixed

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+  var $table = $('table');
+});
+
+function displayOutput() {
+  return $('#displayoutput').val();
+}
+
+function boatGrid(){
+  this.grid = [];
+  for(i = 0; i < 10; i++) {
+    this.grid[i] = [];
+    for(j = 0; j < 10; j++){
+      this.grid[i][j] = [];
+    }
+  }
+  this.drawGrid = function(table, option){
+    
+  };
+}
